e5f2ca88bd147203206c6fdcd2fe85b22a16b48f
VictorVorosAB <victor.voros@aonbenfield.com>
2020-07-28 15:49:07 +0100

    tidied up newsletter and added latest

--- js/common.js
+++ js/common.js
@@ -16,13 +16,88 @@ function collapse(s)
     d.className = "menuNormal";
 }
 
-function loadmarkdown(contenturl, divelement)
+ContentManager = function(contentURL, isJson)
+{
+    function _notifyHandlers(data)
+    {
+        console.log('notifying ' + _loadedHandler.length + ' handlers ' + data);
+        _content = data;
+        for (i=0; i<_loadedHandler.length; i++) {
+        // $.each(_loadedHandler, function(handler) {
+           // console.log(handler);
+            _loadedHandler[i](data);  
+		}
+	}
+
+    function _jsonReviver(key, value)
+    {
+        if (key.toLowerCase().search('date') >= 0)
+        {
+            return new Date(value);  
+		}
+        else
+        {
+            return value;  
+		}
+	}
+
+    var _loadedHandler = [];
+    var _content;
+    if (isJson)
+    {
+        console.log('calling getjson');
+        $.ajax({url:contentURL, datatype:"json", cache:false, success:function(data) { 
+            result = JSON.parse(data, _jsonReviver);
+            _notifyHandlers(result);
+            }});
+        // $.getJSON(contentURL,  _notifyHandlers);
+    }
+    else
+    {
+        $.ajax({url:contentURL, cache:false, success:_notifyHandlers});
+	}
+
+    this.Listen = function(listeningFunc)
+    {
+        if (_content)
+        {
+            console.log('content available already. value ' + _content);
+            listeningFunc(_content);  
+		}
+        else
+        {
+            console.log('content not available. adding to list' + listeningFunc);
+            _loadedHandler.push(listeningFunc);  
+		}
+	}
+}
+
+// var MonthLookup = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+
+function FormatDate(date)
+{
+    var dom = date.getDate();
+    // var dom = date.toLocaleString('default', { day: '2-digit' });
+    var domlastDigit = dom % 10;
+    var domSuffix = domlastDigit == 1 ? "st" : (domlastDigit == 2 ? "nd" : (domlastDigit == 3 ? "rd" : "th"));
+    var month = date.toLocaleString('default', { month: 'long' });
+    return dom + domSuffix + " " +  month + " " + date.getFullYear();
+}
+
+
+function showmarkdown(contentData, divelement)
 {
     var converter = new showdown.Converter(); 
+    html = converter.makeHtml(contentData);
+    document.getElementById(divelement).innerHTML = html;
+}
+
+function loadmarkdown(contenturl, divelement)
+{
+    
     $.get(contenturl, function(response) {
          // alert(response);
-         html = converter.makeHtml(response);
-        document.getElementById(divelement).innerHTML = html;
+         showmarkdown(reponse, divelement);
     }); 
 }
 
@@ -36,6 +111,23 @@ function loadJsonFile(jsonURL)
     return data;
 }
 
+var headline = new ContentManager('../content/headline.md', false);
+var newsletters = new ContentManager('../documents/newsletters/contentlist.txt', true);
+
+function loadmainContent()
+{
+    headline.Listen(function(data){
+         console.log('maincontent got data' + data);
+         showmarkdown(data, 'banner');
+	});
+
+    newsletters.Listen(function(data){
+        console.log('got JSON data' + data);
+        var latestHtml = document.getElementById('latestNewsletter').innerHTML;
+        latestHtml = latestHtml.replace('%LATEST%', data[0].newsFileName);
+        document.getElementById('latestNewsletter').innerHTML = latestHtml;
+	});
+}
 
 
 // initial function called when document loaded to load in common banner, menu etc
@@ -57,6 +149,8 @@ $(function(){
      content.style.display = "block";
      return false;
    }
+
+   loadmainContent();
 
     $('#placedContent').append($("#content"));
     placedContent.style.display = "block";
